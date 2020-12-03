bdf1b84bcde4ec830dee04c5c3baa509a84faf9c
ra101 <ra101@protonmail.com>
2020-12-03 14:57:48 +0530

    Object Spawner

--- Assets/PlacementScript.cs
+++ Assets/PlacementScript.cs
@@ -9,7 +9,7 @@ public class PlacementScript : MonoBehaviour
     private ARRaycastManager rayManager;
     private GameObject HexPlane;
 
-    void Start ()
+    void Start()
     {
         // get the components
         rayManager = FindObjectOfType<ARRaycastManager>();
@@ -18,21 +18,22 @@ public class PlacementScript : MonoBehaviour
         // hide the placement indicator visual
         HexPlane.SetActive(false);
     }
-    
-    void Update ()
+
+    void Update()
     {
         // shoot a raycast from the center of the screen
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
         rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
 
         // if we hit an AR plane surface, update the position and rotation
-        if(hits.Count > 0)
+        if (hits.Count > 0)
         {
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
 
             // enable the visual if it's disabled
-            if(!HexPlane.activeInHierarchy){
+            if (!HexPlane.activeInHierarchy)
+            {
                 HexPlane.SetActive(true);
             }
         }
---
+++ Assets/RAScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RAScript : MonoBehaviour
+{
+
+    public GameObject objectToSpawn;
+    private PlacementScript hex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hex = FindObjectOfType<PlacementScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began){
+            GameObject obj = Instantiate(objectToSpawn, hex.transform.position, hex.transform.rotation);
+        }
+        
+    }
+}
