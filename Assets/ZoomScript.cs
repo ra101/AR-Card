7a568637553080fc96d771fdd093185562f99afd
ra101 <ra101@protonmail.com>
2020-12-06 23:21:14 +0530

    Flicking

--- Assets/ZoomScript.cs
+++ Assets/ZoomScript.cs
@@ -6,22 +6,22 @@ using TMPro;
 public class ZoomScript : MonoBehaviour
 {
 
-    private GameObject raText;
-    private bool wasZoomingLastFrame;
-    private bool zoomed;
+    private TextMeshPro raText;
+    private bool wasZoomingLastFrame, zoomed, flicked;
     private Vector2[] lastZoomPositions;
 
     // Start is called before the first frame update
     void Start()
     {
-        raText = transform.GetChild(0).gameObject;
+        raText = transform.GetChild(0).GetComponent<TextMeshPro>();
         wasZoomingLastFrame = false;
         zoomed = false;
+        flicked = false;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {        
         if (!zoomed && Input.touchCount == 2)
         {
             Vector2[] newPositions = new Vector2[] { Input.GetTouch(0).position, Input.GetTouch(1).position };
@@ -40,11 +40,17 @@ public class ZoomScript : MonoBehaviour
 
                 if(offset > 40){
                     zoomed = true;
-                    raText.GetComponent<TextMeshPro>().text ="<RA>";
+                    raText.text ="<  >";
                 }
 
                 lastZoomPositions = newPositions;
             }
+        }
+
+        
+        if (!flicked && zoomed && Input.acceleration.sqrMagnitude > 5){
+            flicked = true;
+            raText.text ="<RA>";
         }
     }
 }
